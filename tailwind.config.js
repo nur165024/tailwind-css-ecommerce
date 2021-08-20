e8dac678dabe5dfadeb8b4ca0557b48effc54233
nurealam <nur42658@gmail.com>
2021-08-20 07:21:50 +0700

    tailwind css ecommerce my first commit, home design done

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+        roboto: "'Roboto', sans-serif",
+      },
+      colors: {
+        primary: "#fd3d57",
+        secondary: "#324324",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ["group-hover"],
+      visibility: ["group-hover"],
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
